e07c730c408af3cdeaa91c1a26f2d5cb6848264c
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-04-11 18:40:44 +0200

    Mairie christmas DONE

--- lib/mairie_christmas.rb
+++ lib/mairie_christmas.rb
@@ -2,28 +2,53 @@ require 'nokogiri'
 require 'pry'
 require 'open-uri'
 
-
-def get_townhall_email(townhall_url)
-  page = Nokogiri::HTML(open(townhall_url))
-  single_email = page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
-  puts single_email
-end
-
-
-
+### On récupère les urls qui donnent les infos de chaque mairie (=> array)
 def get_townhall_urls(annuaire)
   page = Nokogiri::HTML(open(annuaire))
   array_of_urls = page.xpath('//*[@class="lientxt"]').map do
     |email| "https://www.annuaire-des-mairies.com" + email["href"][1..-1]
   end
+  return array_of_urls
 end
 
-get_townhall_email(get_townhall_urls("https://www.annuaire-des-mairies.com/val-d-oise.html"))
 
-# def industrialize(townhall_directory)
-#   list_of_urls = get_townhall_urls(townhall_directory)
-#   get_townhall_email(list_of_urls)
-# end
+# ### On récupère le nom des mairies (=> array)
+def get_townhall_name(annuaire)
+  page = Nokogiri::HTML(open(annuaire))
+  array_of_townhall_name = page.xpath('//*[@class="lientxt"]').map do
+    |email| email.text
+  end
+  return array_of_townhall_name
+end
 
 
-# industrialize("https://www.annuaire-des-mairies.com/val-d-oise.html")
+# ### On récupère les addresse mail des la mairie (=> array)
+def get_townhall_email(townhall_urls)
+  array_of_emails = []
+  townhall_urls.each do |url|
+    page = Nokogiri::HTML(open(url))
+    array_of_emails << page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+  end
+  return array_of_emails
+end
+
+
+def industrialize(annuaire)
+  # appel de la fonction qui renvoit un array d'url
+  array_of_urls = get_townhall_urls(annuaire)
+
+  # fonction qui renvoit les noms des villes
+  array_of_townhall_name = get_townhall_name(annuaire)
+
+  #fonction qui renvoit un array d'email
+  array_of_emails = get_townhall_email(array_of_urls)
+
+  #hash avec le nom des villes en clé, et les mails en valeur. Enfin on split en minihash
+  christmas_hash = array_of_townhall_name.zip(array_of_emails).to_h
+  return christmas_hash.each_slice(1).map(&:to_h)
+end
+
+
+puts industrialize("https://www.annuaire-des-mairies.com/val-d-oise.html")
+
+/html/body/div[3]/div/div/section/div/article/div[3]/div/div[3]/div[1]/ul[1]/li[1]/a
