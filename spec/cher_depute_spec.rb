119154900c53345d32cc95b2e63580af8128fcf1
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-04-12 00:59:14 +0200

    all specs added

---
+++ spec/cher_depute_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../lib/cher_depute'
+
+describe "the cher_depute function" do
+  it "should return client, and client is not nil" do
+    expect(industrialize).not_to be_nil
+  end
+end
--- spec/mairie_christmas_spec.rb
+++ spec/mairie_christmas_spec.rb
@@ -1,8 +1,19 @@
-require_relative '../lib/dark_trader'
+require_relative '../lib/mairie_christmas'
 
-
-describe "the dark_trader function" do
+describe "the industrialize function" do
   it "should return client, and client is not nil" do
-    expect(dark_trader).not_to be_nil
+    expect(industrialize("https://www.annuaire-des-mairies.com/val-d-oise.html")).not_to be_nil
+  end
+end
+  # it "should return a hash with the city and the email adress" do
+  #   expect(industrialize("https://www.annuaire-des-mairies.com/val-d-oise.html")[5]["ARGENTEUIL"]).to eq({"ARGENTEUIL"=>"christian.bournery@ville-argenteuil.fr"})
+  #   expect(get_townhall_email(get_townhall_urls("https://www.annuaire-des-mairies.com/val-d-oise.html"))[179]["VILLERON"]).to eq({"VILLERON"=>"accueilvilleron@roissy-online.com"})
+  # end
+
+describe "the get_townhall_email function" do
+  it "should return valid emails" do
+    expect(get_townhall_email(get_townhall_urls("https://www.annuaire-des-mairies.com/val-d-oise.html"))).should include("mairie.amenucourt@wanadoo.fr")
+    expect(get_townhall_email(get_townhall_urls("https://www.annuaire-des-mairies.com/val-d-oise.html"))).should include("mairiefremecourt@orange.fr")
+    expect(get_townhall_email(get_townhall_urls("https://www.annuaire-des-mairies.com/val-d-oise.html"))).should include("communication@ville-osny.fr")
   end
 end
